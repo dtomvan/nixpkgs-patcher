466da00ff2d63ddded5bc054b90b8c957d911eca
Tom van Dijk <18gatenmaker6@gmail.com>
2026-04-25 14:49:45 +0200

    initial commit

---
+++ flake.nix
@@ -0,0 +1,71 @@
+{
+  description = "Patching nixpkgs";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs-patched.url = "github:dtomvan/nixpkgs/nixos-unstable-patched";
+
+    nixpkgs-patch-10 = {
+      url = "https://github.com/NixOS/nixpkgs/pull/512989.patch";
+      flake = false;
+    };
+
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    systems.url = "github:nix-systems/default";
+    nix-patcher = {
+      url = "github:katrinafyi/nix-patcher";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = import inputs.systems;
+
+      perSystem =
+        {
+          pkgs,
+          lib,
+          self',
+          inputs',
+          ...
+        }:
+        {
+          packages.default = pkgs.callPackage (
+            {
+              lib,
+              writeShellApplication,
+              opts ? { },
+            }:
+            writeShellApplication {
+              name = "nix-patcher";
+              runtimeInputs = lib.singleton inputs'.nix-patcher.packages.default;
+              text = ''
+                nix-patcher ${
+                  lib.cli.toCommandLineShellGNU { } (
+                    {
+                      flake = ".";
+                      commit = true;
+                      patched-suffix = "-patched";
+                      upstream-suffix = "";
+                    }
+                    // opts
+                  )
+                }
+              '';
+            }
+          ) { };
+          devShells.default = pkgs.mkShellNoCC {
+            packages = lib.singleton self'.packages.default;
+            shellHook = ''
+              # intentionally print auth status so the user knows how they are
+              # logged in and where they will patch their nixpkgs to
+              if type -a gh 2>&1 >/dev/null && gh auth status; then
+                export GITHUB_TOKEN="$(gh auth token)"
+              fi
+            '';
+          };
+        };
+    };
+}
